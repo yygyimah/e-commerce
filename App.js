346e68dccf9f316250d22e6b78c8b9ca0f817cb5
Yaw Gyimah <owusugyimahyaw@gmail.com>
2021-11-01 22:10:48 +0000

    working on login screen

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-       <Text style={{color: '#888', fontSize: 54}}>
-          Hello World!
-          </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [name, setName] = useState('kwame')
+    const [age, setAge] = useState(10)
+
+    const clickHandler = () => {
+        setName('Alex freeman')
+    }
+
+    return (
+       <View style={styles.container}>
+           <Text>Enter name:</Text>
+           <TextInput
+               style={styles.input}
+               placeholder='eg. John Doe'
+               onChangeText={(val) => {setName(val)}}
+           />
+
+           <Text>Enter age:</Text>
+           <TextInput
+               style={styles.input}
+               placeholder='eg. 10'
+               onChangeText={(val) => {setAge(val)}}
+               keyboardType='numeric'
+           />
+           <Text>name: {name}, age: {age}</Text>
+
+       </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        marginTop: 20
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#777',
+        padding: 8,
+        margin: 10,
+        width: 200
+    }
+})
